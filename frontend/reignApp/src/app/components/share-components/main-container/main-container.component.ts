07eb6a1d11da672a0792f137b42236fc6bd81f3d
rabindranath ferreira villamizar <rabindranathucv@gmail.com>
2019-12-28 14:01:34 -0300

    no data articles, add articles service, auth service, session service

--- frontend/reignApp/src/app/components/share-components/main-container/main-container.component.ts
+++ frontend/reignApp/src/app/components/share-components/main-container/main-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from 'src/app/services/articles/articles.service';
 
 @Component({
   selector: 'app-main-container',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor( private articlesService: ArticlesService) { }
 
   ngOnInit() {
+    this.getArticles();
   }
+
+  /**
+   * getArticles
+   */
+  public getArticles() {
+    console.log('ejecutandose');
+    this.articlesService.getArticles().subscribe( (resp: any) => {
+      console.log(resp);
+    });
+  }
+
+
 
 }

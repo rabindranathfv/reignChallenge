35ebf3a4ebdbc8fc7bc68c81a503a1bd2bbe5fec
Unknown <ferreiralox@gmail.com>
2019-12-29 19:25:58 -0300

    automate build for Dev

--- backend/reignApi/src/server/config/config.js
+++ backend/reignApi/src/server/config/config.js
@@ -7,54 +7,20 @@ console.log('load node process env', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'dev') {
     console.log('running dev');
     dotenv.config({ path: path.join(__dirname, './.env') });
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.DB_HOST);
-    console.log(process.env.HOST);
-    console.log(process.env.PORT);
-    console.log(process.env.DB_NAME);
-    console.log(process.env.DB_PORT);
-    console.log(process.env.SEED);
-    console.log(process.env.API_URL);
-    console.log(process.env.URLDB);
-    console.log(process.env.TIME_TOKEN);
-
     // ######## token time
-    // 60 min
-    // 60 seg
-    // 24 hours
-    // 30 days
     // process.env.TIME_TOKEN = "24h";
-
     // ###########  BD Config
     process.env.URLDB = process.env.MONGO_URI ? process.env.MONGO_URI : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 } else if (process.env.NODE_ENV === 'staging') {
     console.log('running staging');
     console.log(path.join(__dirname, './.env.staging'));
     dotenv.config({ path: path.join(__dirname, './.env.staging') });
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.DB_HOST);
-    console.log(process.env.HOST);
-    console.log(process.env.PORT);
-    console.log(process.env.DB_NAME);
-    console.log(process.env.DB_PORT);
-    console.log(process.env.SEED);
-    console.log(process.env.API_URL);
-    console.log(process.env.URLDB);
-    console.log(process.env.TIME_TOKEN);
+    // ###########  BD Config
     process.env.URLDB = process.env.MONGO_URI ? process.env.MONGO_URI : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 } else {
     console.log('running production');
     console.log(path.join(__dirname, './.env.prod'));
     dotenv.config({ path: path.join(__dirname, './.env.prod') });
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.DB_HOST);
-    console.log(process.env.HOST);
-    console.log(process.env.PORT);
-    console.log(process.env.DB_NAME);
-    console.log(process.env.DB_PORT);
-    console.log(process.env.SEED);
-    console.log(process.env.API_URL);
-    console.log(process.env.URLDB);
-    console.log(process.env.TIME_TOKEN);
+    // ###########  BD Config
     process.env.URLDB = process.env.MONGO_URI ? process.env.MONGO_URI : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 }

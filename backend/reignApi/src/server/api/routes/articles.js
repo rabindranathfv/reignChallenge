fa901c24bfaeedce4ecc36ab2d5830224a171b40
rabindranath ferreira villamizar <rabindranathucv@gmail.com>
2019-12-31 20:43:13 -0300

    delete test endpoint for docker comose

--- backend/reignApi/src/server/api/routes/articles.js
+++ backend/reignApi/src/server/api/routes/articles.js
@@ -11,10 +11,6 @@ const app = express();
 1.-checkToken
 
 */
-app.get('/test', (req, res) => {
-    res.json({ message: 'pass!' });
-});
-
 app.get('/v1/articles', articleCtrl.getArticles);
 
 cron.schedule('0 0 */1 * * *', articleCtrl.getArticlesRemote);
